88446e9b12dc590478fee3edb7df0cbcde655e71
Yasin Tohan <yasintohann@gmail.com>
2021-03-11 17:37:27 +0300

    composer added

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'cc7d977db45fc3fa990e995a408cc1cd37dfdcfd',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'cc7d977db45fc3fa990e995a408cc1cd37dfdcfd',
+    ),
+  ),
+);
